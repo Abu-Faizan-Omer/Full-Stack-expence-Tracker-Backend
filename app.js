44f99cbfefd02fbfdfe961d32a69c33a6d3b87cf
Abu-Faizan-Omer <abufaizan1997@gmail.com>
2024-11-28 15:14:42 +0530

    Forgot Password & Mailing & Reset Password Link

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const userRoutes=require("./routes/user")
 const expenceroutes=require("./routes/expence")
 const purchaseroutes=require("./routes/purchase")
 const premiumFeatureRoutes = require('./routes/premiumFeature')
+const resetPasswordRoutes=require("./routes/resetPassword")
 
 const User=require("./models/user")
 const Expences = require("./models/expence")
 const Order=require("./models/order")
+const Forgotpassword=require("./models/forgotpasswordm")
 
 app.use(cors());
 app.use(bodyparser.urlencoded({extended:false}))
@@ -23,12 +25,16 @@ app.use("/users",userRoutes)
 app.use("/expence",expenceroutes)
 app.use("/purchase",purchaseroutes)
 app.use('/premium', premiumFeatureRoutes)
+app.use('/password',resetPasswordRoutes)
 
 User.hasMany(Expences)
 Expences.belongsTo(User)
 
 User.hasMany(Order)
 Order.belongsTo(User)
+
+User.hasMany(Forgotpassword);
+Forgotpassword.belongsTo(User);
 
 sequelize.sync()
 .then((result)=>{
